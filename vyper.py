ae2cf37ef4adc21d82c0820a97d90ff84b193d6b
ThiwakonPB <thiwakonmezenen@gmail.com>
2021-12-15 14:00:19 +0700

    Add target option for vyper.py

--- vyper.py
+++ vyper.py
@@ -1,7 +1,7 @@
 
 import os
 import sys
-
+import yaml
 if sys.argv[1] == "compile":
     os.system("vyper ./vyper_src/" + sys.argv[2] + " > ./config/bytecode.txt")
     os.system("vyper -f abi_python ./vyper_src/" + sys.argv[2] + " > ./config/abi.txt")
@@ -9,4 +9,17 @@ if sys.argv[1] == "compile":
 elif sys.argv[1] == "deploy":
     os.system("vyper ./vyper_src/" + sys.argv[2] + " > ./config/bytecode.txt")
     os.system("vyper -f abi_python ./vyper_src/" + sys.argv[2] + " > ./config/abi.txt")
-    
+
+elif sys.argv[1] == "nodeip":
+    with open('./config/config.yaml') as database:
+        data = yaml.safe_load((database))
+        data['nodeip'] = sys.argv[2]
+    with open("./config/config.yaml", "w") as f:
+        yaml.dump(data, f)
+
+elif sys.argv[1] == "private_key":
+    with open('./config/config.yaml') as database:
+        data = yaml.safe_load((database))
+        data['private_key'] = sys.argv[2]
+    with open("./config/config.yaml", "w") as f:
+        yaml.dump(data, f)
