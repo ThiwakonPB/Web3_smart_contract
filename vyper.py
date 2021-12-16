98ffcad3692970ab30dd1602dc21f8c4818310ff
beatmil <anutorn@gmail.com>
2021-12-16 12:39:08 +0700

    Check for python3 and python before execute

--- vyper.py
+++ vyper.py
@@ -1,7 +1,27 @@
-
 import os
 import sys
 import yaml
+
+is_python3 = False
+
+def check_python():
+    status = os.system(f"command -v python3")
+    if status != 0:
+        print(f"python3: not found")
+        print(f"Trying python...")
+        status2 = os.system(f"command -v python")
+        if status2 != 0:
+            print(f"python: not found")
+            print("Abort")
+            exit()
+        else:
+            print(f"Detect: python")
+    else:
+        print(f"python3: detected")
+        global is_python3 
+        is_python3 = True
+
+
 if sys.argv[1] == "compile":
     os.system("vyper ./vyper_src/" + sys.argv[2] + " > ./config/bytecode.txt")
     os.system("vyper -f abi_python ./vyper_src/" + sys.argv[2] + " > ./config/abi.txt")
@@ -11,6 +31,7 @@ if sys.argv[1] == "compile":
     print(f2.read())
     
 elif sys.argv[1] == "deploy":
+    check_python()
     os.system("vyper ./vyper_src/" + sys.argv[2] + " > ./config/bytecode.txt")
     os.system("vyper -f abi_python ./vyper_src/" + sys.argv[2] + " > ./config/abi.txt")
     try:
@@ -23,7 +44,10 @@ elif sys.argv[1] == "deploy":
                 yaml.dump(data, f)
     except:
         print("No arguments provided")
-    os.system("python ./python_src/deploy-from-yaml.py")
+    if is_python3:
+        os.system("python3 ./python_src/deploy-from-yaml.py")
+    else:
+        os.system("python ./python_src/deploy-from-yaml.py")
 
 elif sys.argv[1] == "nodeip" or sys.argv[1] == "private_key" or sys.argv[1] == "contract_address" or sys.argv[1] == "gas":
     key = sys.argv[1]
