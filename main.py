60c141aab962dfeabfedc7c2c11dde800280d28c
beatmil <anutorn@gmail.com>
2022-01-19 17:09:30 +0700

    Add deploy to typer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import typer
 import os
 import yaml
+from web3 import Web3
+from web3 import HTTPProvider
 
 
 app = typer.Typer()
@@ -106,9 +108,75 @@ def load():
 
 
 @app.command()
-def deploy():
+def deploy(contract_name: str):
     """
     Deploy a contract
     """
     typer.echo("Deploy a contract!")
 
+    # get config yaml
+    with open("config/config.yaml", "r") as stream:
+        try:
+            data = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    # get abi and bytecode
+    with open(f"data/{contract_name}", "r") as stream:
+        box_bytecode = stream.readline()[:-1]
+        box_abi = stream.readline()[:-1]
+    
+    # connect to node
+    w3 = Web3(HTTPProvider(data["nodeip"]))
+
+    # check connection
+    is_connect = w3.isConnected()
+    if not is_connect:
+        typer.echo("Connection: {is_connect}")
+        raise Exception("Can't connect to node")
+    typer.echo(f"IP: {data['nodeip']}")
+    typer.echo(f"Connection: {is_connect}")
+
+
+    # get account by private key
+    acct = w3.eth.account.privateKeyToAccount(data["private_key"])
+    print("Address: ", acct.address)
+
+
+    # create contract with abi and bytecode
+    contract = w3.eth.contract(
+        abi=box_abi,
+        bytecode=box_bytecode
+    )
+
+
+    # check if there are agrs or not
+    if data["args"] == None:
+        construct_txn = contract.constructor().buildTransaction({
+            'from': acct.address,
+            'nonce': w3.eth.getTransactionCount(acct.address),
+            'value': w3.toWei(str(data["value"]), 'ether'),
+            'gas': int(data["gas"]),
+            'gasPrice': int(data["gasPrice"])})
+    else:
+        construct_txn = contract.constructor(*data["args"]).buildTransaction({
+            'from': acct.address,
+            'nonce': w3.eth.getTransactionCount(acct.address),
+            'value': w3.toWei(str(data["value"]), 'ether'),
+            'gas': int(data["gas"]),
+            'gasPrice': int(data["gasPrice"])})
+
+
+    signed = acct.signTransaction(construct_txn)    
+    tx_hash = w3.eth.sendRawTransaction(signed.rawTransaction)
+    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    print(tx_receipt)
+    print("---------------------------------")
+    print(f"Contract Address: {tx_receipt['contractAddress']}")
+    print("---------------------------------")
+    # os.system("echo 'YAY'")
+    # os.system(f"echo '{tx_receipt['contractAddress']}' >> data/{contract}")
+
+    # os.system("echo 'MAMAMAM' >> data/kazuma.vy")
+    print("ECHO HERE")
+    os.system(f"echo {tx_receipt['contractAddress']} >> data/{contract_name}")
